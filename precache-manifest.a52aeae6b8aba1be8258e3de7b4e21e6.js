ef2ba5026d20c270c075ea725ee30ad748e115b5
hgko1207 <khkkhk1207@gmail.com>
2020-03-20 14:13:21 +0900

    Updates

---
+++ precache-manifest.a52aeae6b8aba1be8258e3de7b4e21e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb72bf9dea166c68bd92b26132e43fc",
+    "url": "/gmail-client/index.html"
+  },
+  {
+    "revision": "c26ae183f42bed3304df",
+    "url": "/gmail-client/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "fa1d5f416ff0f86df8a2",
+    "url": "/gmail-client/static/js/2.e4684a4c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/gmail-client/static/js/2.e4684a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c26ae183f42bed3304df",
+    "url": "/gmail-client/static/js/main.26fa6ec7.chunk.js"
+  },
+  {
+    "revision": "8a801a202e24d5572a20",
+    "url": "/gmail-client/static/js/runtime-main.a6cb5669.js"
+  }
+]);
